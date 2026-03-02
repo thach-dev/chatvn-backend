6f3fae73f824e09baf3bddbb362795bd3ec76152
thach-le <lehoangthach2k2@gmail.com>
2026-03-02 20:23:28 +0700

    move express into api index

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-import app from "../app.js";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend running 🚀" });
+});
 
 export default app;
